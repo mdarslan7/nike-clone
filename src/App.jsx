14c974452d48ec0f59e1c77e691e633e4f80718b
mdarslan7 <mdarslan7@gmail.com>
2023-09-12 16:44:34 +0530

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="max-w-sm m-4 p-3 font-monospace rounded-md shadow-xl ring-2 ring-slate-100">
+      <h3 className="font-medium">Md Arslan</h3>
+      <span>Full Stack Developer </span>
+      <span>@webcrux</span>
+    </div>
+  )
+}
