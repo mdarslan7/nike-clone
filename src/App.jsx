efad037c3f6c1d94fb753ef917520ae3cae1317e
mdarslan7 <mdarslan7@gmail.com>
2023-09-13 12:28:37 +0530

    changed index.css and tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ export default function App() {
   return (
     <div className="max-w-sm m-4 p-3 font-monospace rounded-md shadow-xl ring-2 ring-slate-100">
       <h3 className="font-medium">Md Arslan</h3>
-      <span>Full Stack Developer </span>
-      <span>@webcrux</span>
+      <span>Full Stack Developer</span>
+      <span className="italic">@webcrux</span>
     </div>
   )
 }
